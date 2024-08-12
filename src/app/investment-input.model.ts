96aff4d146e8f0ec581c2650bd9f9c8d897175b2
Alexander <drumpoltava@gmail.com>
2024-08-12 15:52:14 +0300

    send to git hub

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,8 @@
+export  interface InvestmentInput {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
+
+
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import {Component, signal, inject} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+import {InvestmentService} from "../investment-service";
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.scss'
+})
+export class UserInputComponent {
+  private investmentService = inject(InvestmentService);
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
+    })
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
